83e9f9570ff115897994781027cf2d64da3fb450
Blue <leanefelis@gmail.com>
2021-08-31 11:43:02 +0200

    ajout changement de thème, ça ne fonctionne pas

---
+++ Portfolio/Tests/Mode/mode.js
@@ -0,0 +1,12 @@
+//Switch light/dark
+	
+$(".switch").on('click', function () {
+    if ($("body").hasClass("light")) {
+        $("body").removeClass("light");
+        $(".switch").removeClass("switched");
+    }
+    else {
+        $("body").addClass("light");
+        $(".switch").addClass("switched");
+    }
+});
