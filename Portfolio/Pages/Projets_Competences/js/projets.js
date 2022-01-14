9d1f0dd804ec0c82b53634b0b8cbfecf79c46990
Blue <leanefelis@gmail.com>
2022-01-14 14:35:57 +0100

    ajout structure + js

--- Portfolio/Pages/Projets_Competences/js/projets.js
+++ Portfolio/Pages/Projets_Competences/js/projets.js
@@ -34,3 +34,13 @@
         })
     });
 })(jQuery);
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "100%";
+    // document.getElementById("main").style.marginLeft = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    // document.getElementById("main").style.marginLeft = "0";
+}
